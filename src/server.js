687c497b0999355d174efda0605745efbf21712c
Joaquim <joaquimoliart123@gmail.com>
2025-09-29 18:51:58 -0300

    Feat: servidor com rotas e verifiacador

--- src/server.js
+++ src/server.js
@@ -1,3 +1,15 @@
 import express from "express";
+import userRoute from "./routes/user.js";
+import bookRoute from "./routes/books.js";
+import verifyUser from "./middleware/verify-user.js";
 
+const app = express();
 app.use(express.json());
+
+// Rotas
+app.use("/users", verifyUser, userRoute);
+app.use("/books", verifyUser, bookRoute);
+
+app.listen(3000, () => {
+    console.log("Servidor rodando na porta 3000");
+});
